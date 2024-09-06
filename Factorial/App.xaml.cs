560a37610a6f9c55b2e4156c31f89bbfaf703094
junggeon00 <jk001216@naver.com>
2024-09-06 16:16:43 +0900

    Add files via upload

---
+++ Factorial/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Factorial
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
